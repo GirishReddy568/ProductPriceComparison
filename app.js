e64914ed74ef97e481aaff6001e85429ecef2dcb
Akshay0019 <akshayprakash0069@gmail.com>
2025-08-05 13:27:49 +0530

    Fix views path and add EJS files

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-const express=require("express")
-const bodyParser=require('body-parser');
-const app=express();
-let ejs = require('ejs');
-app.set('view engine', 'ejs')
-const a=[]
-let i=0;
-app.listen(3000,()=>{
-    console.log("server is running successfully");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-})
-app.use(bodyParser.urlencoded({ extended: false }))
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/',(req,res)=>{
-    res.render('index',{name:""})
-})
-app.post('/',(req,res)=>{
-    a.push(req.body.name);
-    
-    setTimeout(() => {
-    res.render('product',{name:a[0]})    
-    }, 10000);
+const a = [];
 
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("index", { name: "" });
+});
+
+app.post("/", (req, res) => {
+  a.push(req.body.name);
+
+  setTimeout(() => {
+    res.render("product", { name: a[0] });
+  }, 10000);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
